5b8d1e7838e18fdb89730114b865c4259717e84b
https://EngKen-Peter <kennedymbaluka23@gmail.com>
2025-07-20 18:22:55 +0300

    Fix Mongo URI

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import bugRoutes from './routes/bugs.js';
+import bugRoutes from "./routes/bug.routes.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/bugs', bugRoutes);
+app.use("/api/bugs", bugRoutes);
 
-// Root
-app.get('/', (req, res) => {
-  res.send('Hello from the deployed app!');
+app.get("/", (req, res) => {
+  res.send("Bug Tracker API is running.");
 });
 
-// Connect to DB & start server
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
-  .catch((err) => console.error(err));
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
